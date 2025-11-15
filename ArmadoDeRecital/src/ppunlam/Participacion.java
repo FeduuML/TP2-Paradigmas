c2f2eb336771841d87d6a8e2329e42bba9c2a16b
nntomi <nielsentomasn@gmail.com>
2025-11-15 19:32:49 -0300

    Create Participacion.java

---
+++ ArmadoDeRecital/src/ppunlam/Participacion.java
@@ -0,0 +1,25 @@
+package ppunlam;
+
+public class Participacion {
+	private Artista artista;
+	private Cancion cancion; 
+	private String rol;
+	
+	public Participacion(Artista artista, Cancion cancion, Rol rol) {
+		this.artista = artista;
+		this.cancion = cancion;
+		this.rol = rol;
+	} 
+	
+	public Boolean verificarMaxCanciones(Artista a) {
+		if(a.getActualCanciones() >= a.getMaxCanciones())
+			return false; // no puede
+		else
+			return true; 
+	}
+	
+	
+	
+	
+	
+}
