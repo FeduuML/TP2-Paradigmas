fa014e8c0c2d78d759c2ed054f15376937d04bc7
FeduuML <fedeliber@live.com>
2025-11-15 17:39:24 -0300

    Cambio a Maven Project
    Importamos la libreria Json-simple

--- ArmadoDeRecital/src/ppunlam/App.java
+++ ArmadoDeRecital/src/ppunlam/App.java
@@ -1,5 +1,12 @@
 package ppunlam;
 
+import java.io.FileReader;
+import java.util.Iterator;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 public class App {
 	public static void main(String[] args) {
 		
