9903f507d29e5ba02c5a0c9bb3dc35bf4d083aad
FeduuML <fedeliber@live.com>
2025-11-15 19:32:50 -0300

    Mapeo del JSON a la clase
    Artista

--- ArmadoDeRecital/src/ppunlam/Artista.java
+++ ArmadoDeRecital/src/ppunlam/Artista.java
@@ -9,12 +9,54 @@ import java.util.Objects;
 
 public class Artista {
 	String nombre;
-	List<Rol> roles = new LinkedList<>();
-	List<Banda> bandas =new LinkedList<>();
-	double costo;
-	int actualCanciones;
+	List<String> roles = new LinkedList<>();
+	List<String> bandas = new LinkedList<>();
+	int costo;
+	int actualCanciones = 0;
 	int maxCanciones;
+	
+	public Artista(String nombre, List<String> roles, List<String> bandas, int costo, int maxCanciones) {
+		this.nombre = nombre;
+		this.roles = roles;
+		this.bandas = bandas;
+		this.costo = costo;
+		this.maxCanciones = maxCanciones;
+	}
+
+	public static List<Artista> cargarArtistas(String path) {
+	    List<Artista> artistas = new LinkedList<>();
+
+	    JSONArray array = LectorJSON.cargarArray(path);
+
+	    for (Object o : array) {
+	        JSONObject jsonArtista = (JSONObject) o;
+	        artistas.add(jsonToArtista(jsonArtista));
+	    }
+
+	    return artistas;
+	}
+
+	public static Artista jsonToArtista(JSONObject json) {		
+		String nombre = (String) json.get("nombre");
 		
+	    JSONArray rolesArray = (JSONArray) json.get("roles");
+	    List<String> roles = new LinkedList<>();
+	    for(Object r : rolesArray) {
+	    	roles.add((String) r);
+	    }
+	    
+	    JSONArray bandasArray = (JSONArray) json.get("bandas");
+	    List<String> bandas = new LinkedList<>();
+	    for(Object r : bandasArray) {
+	    	bandas.add((String) r);
+	    }
+	    
+	    int costo = ((Long) json.get("costo")).intValue();
+	    int maxCanciones = ((Long) json.get("maxCanciones")).intValue();
+	    
+	    return new Artista(nombre, roles, bandas, costo, maxCanciones);
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(nombre);
@@ -31,23 +73,9 @@ public class Artista {
 		Artista other = (Artista) obj;
 		return Objects.equals(nombre, other.nombre);
 	}
-		
-	public static void mostrar(JSONArray listaArtistas) {
-		for(Object o : listaArtistas) {
-			JSONObject artista = (JSONObject) o;
-			
-			String nombre = (String) artista.get("nombre");
-		    JSONArray roles = (JSONArray) artista.get("roles");
-		    JSONArray bandas = (JSONArray) artista.get("bandas");
-		    Long costo = (Long) artista.get("costo");
-		    Long maxCanciones = (Long) artista.get("maxCanciones");
-		    
-			System.out.println("Nombre: " + nombre);
-	        System.out.println("Costo: " + costo);
-	        System.out.println("Max Canciones: " + maxCanciones);
-	        System.out.println("Roles: " + roles);
-	        System.out.println("Bandas: " + bandas);
-	        System.out.println("----------------------------");
-		}
-	}
+	
+	@Override
+	public String toString() {
+        return "Nombre: " + nombre + "\nRoles: " + roles + "\nBandas: " + bandas + "\nCosto: " + costo + "\nMaxCanciones: " + maxCanciones + "\n";
+    }
 }
--- ArmadoDeRecital/src/ppunlam/ArtistaExterno.java
+++ ArmadoDeRecital/src/ppunlam/ArtistaExterno.java
@@ -1,6 +1,13 @@
 package ppunlam;
 
+import java.util.List;
+
 public class ArtistaExterno extends Artista{
+	public ArtistaExterno(String nombre, List<String> roles, List<String> bandas, int costo, int maxCanciones) {
+		super(nombre, roles, bandas, costo, maxCanciones);
+		// TODO Auto-generated constructor stub
+	}
+
 	public void entrenar() {
 		
 	}
